77aca30496ebc9d90f7534eda3ecf4b305f2404c
Elhadedy36 <elhadede36@gmail.com>
2024-11-21 04:48:30 +0200

    create column on auth

--- github_test/lib/features/auth/auth_screen.dart
+++ github_test/lib/features/auth/auth_screen.dart
@@ -5,6 +5,10 @@ class AuthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Container(),
+      ],
+    );
   }
 }
